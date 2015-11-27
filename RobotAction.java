a02fa284e28bfca4704669216fdcec5b9bce199f
LiliMeng <lilimeng1103@gmail.com>
2015-11-26 22:02:18 -0800

    Update RobotAction.java

--- RobotAction.java
+++ RobotAction.java
@@ -4,11 +4,12 @@ public class RobotAction
 {
 	public static final int Ahead = 0;
 	public static final int Back = 1;
-	public static final int TurnLeft = 2;
-	public static final int TurnRight = 3;
+	public static final int TurnLeftAhead = 2;
+	public static final int TurnRightAhead = 3;
+	public static final int Fire = 4;
 	
-	public static final int numRobotActions = 4;
+	public static final int numRobotActions = 5;
 	
-	public static final double RobotMoveDistance = 400.0;
-	public static final double RobotTurnDegree = 45.0;
+	public static final double RobotMoveDistance = 300.0;
+	public static final double RobotTurnDegree = 90.0;
 }
