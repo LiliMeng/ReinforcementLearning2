23b49a56e7cb7323ba5692bea9771590ac1faa76
LiliMeng <lilimeng1103@gmail.com>
2015-11-26 22:01:59 -0800

    Update LUQtable.java

--- LUQtable.java
+++ LUQtable.java
@@ -1,8 +1,19 @@
 package ReinforcementLearning;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+
+
 import ReinforcementLearning.RobotAction;
 import ReinforcementLearning.RobotState;
 
+import robocode.*; 
+
+import robocode.AdvancedRobot; 
 
 public class LUQTable {
 	
@@ -20,14 +31,15 @@ public class LUQTable {
 		{
 			for(int j=0; j<RobotAction.numRobotActions; j++)
 			{
-				qTable[i][j] = 0.0;
+				qTable[i][j] = 0;
 			}
 		}
 	}
 	
 	public double maxQValue(int state)
 	{
-		double maxQvalue = 0.0;
+		double maxQvalue = qTable[state][0];
+		
 		for(int i=0; i<qTable[state].length;i++)
 		{	
 			if(qTable[state][i] > maxQvalue)
@@ -40,7 +52,8 @@ public class LUQTable {
 	
 	public int bestAction(int state)
 	{
-		double maxQvalue = 0.0;
+		double maxQvalue = qTable[state][0];
+		
 		int bestAct = 0;
 		for(int i=0; i<qTable[state].length; i++)
 		{
@@ -78,5 +91,69 @@ public class LUQTable {
 		return sum;
 	}
 	
+	 public void loadData(File file)   
+	  {   
+	    BufferedReader r = null;   
+	    try   
+	    {   
+	      r = new BufferedReader(new FileReader(file));   
+	      for (int i = 0; i < RobotState.numStates; i++)   
+	        for (int j = 0; j < RobotAction.numRobotActions; j++)   
+	          qTable[i][j] = Double.parseDouble(r.readLine());   
+	    }   
+	    catch (IOException e)   
+	    {   
+	      System.out.println("IOException trying to open reader: " + e);   
+	      initializeQtable();   
+	    }   
+	    catch (NumberFormatException e)   
+	    {   
+	      initializeQtable();   
+	    }   
+	    finally   
+	    {   
+	      try   
+	      {   
+	        if (r != null)   
+	          r.close();   
+	      }   
+	      catch (IOException e)   
+	      {   
+	        System.out.println("IOException trying to close reader: " + e);   
+	      }   
+	    }   
+	  }   
+	
+	 public void saveData(File file)   
+	  {   
+	    PrintStream w = null;   
+	    try   
+	    {   
+	      w = new PrintStream(new RobocodeFileOutputStream(file));   
+	      for (int i = 0; i < RobotState.numStates; i++)   
+	        for (int j = 0; j < RobotAction.numRobotActions; j++)   
+	          w.println(new Double(qTable[i][j]));   
+	   
+	      if (w.checkError())   
+	        System.out.println("Could not save the data!");   
+	      w.close();   
+	    }   
+	    catch (IOException e)   
+	    {   
+	      System.out.println("IOException trying to write: " + e);   
+	    }   
+	    finally   
+	    {   
+	      try   
+	      {   
+	        if (w != null)   
+	          w.close();   
+	      }   
+	      catch (Exception e)   
+	      {   
+	        System.out.println("Exception trying to close witer: " + e);   
+	      }   
+	    }   
+	  }    
 	
 }
