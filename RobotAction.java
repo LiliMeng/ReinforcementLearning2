c836eaa739853d8c02fb2719cc602d638aee0ba6
LiliMeng <lilimeng1103@gmail.com>
2015-11-27 21:04:45 -0800

    Update RobotAction.java

--- RobotAction.java
+++ RobotAction.java
@@ -2,14 +2,18 @@ package ReinforcementLearning;
 
 public class RobotAction
 {
-	public static final int Ahead = 0;
+	/*public static final int Ahead = 0;
 	public static final int Back = 1;
 	public static final int TurnLeftAhead = 2;
 	public static final int TurnRightAhead = 3;
-	public static final int Fire = 4;
+	//public static final int Fire = 4;
+	*/
+	public static final int AntiGravityMove=0;
+	public static final int GravityMove=1;
+	public static final int AimAndFire= 2;
 	
-	public static final int numRobotActions = 5;
+	public static final int numRobotActions = 3;
 	
 	public static final double RobotMoveDistance = 300.0;
-	public static final double RobotTurnDegree = 90.0;
+	public static final double RobotTurnDegree = 45.0;
 }
