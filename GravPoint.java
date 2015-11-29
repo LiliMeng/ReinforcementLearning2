bde3b59881e7e35f317f4fabeb72efcb7d25b01e
LiliMeng <lilimeng1103@gmail.com>
2015-11-29 09:26:51 -0800

    Create GravPoint.java

---
+++ GravPoint.java
@@ -0,0 +1,13 @@
+package ReinforcementLearning;
+
+/**Holds the x, y, and strength info of a gravity point**/
+public class GravPoint {
+	
+    public double x,y,power;
+    
+    public GravPoint(double pX,double pY,double pPower) {
+        x = pX;
+        y = pY;
+        power = pPower;
+    }
+}
